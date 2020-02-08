006a1434404ac51a2867e6eaa2dec85928098208
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-02-09 12:37:31 +1300

    added win and lose screen

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    [SerializeField] GameObject winLabel;
+    [SerializeField] GameObject loseLabel;
+    [SerializeField] float waitToLoad = 4f;
     int numberOfAttackers = 0;
     bool levelTimerFinished = false;
-    
+
+    private void Start()
+    {
+        winLabel.SetActive(false);
+        loseLabel.SetActive(false);
+    }
+
     public void AttackerSpawned()
     {
         numberOfAttackers++;
@@ -17,8 +26,22 @@ public class LevelController : MonoBehaviour
         numberOfAttackers--;
         if (numberOfAttackers <= 0 && levelTimerFinished)
         {
-            Debug.Log("End level now");
+            StartCoroutine(HandleWinCondition());
         }
+    }
+
+    IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(waitToLoad);
+        FindObjectOfType<LevelLoader>().LoadNextScene();
+    }
+
+    public void HandleLoseCondition()
+    {
+        loseLabel.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void LevelTimerFinished()
